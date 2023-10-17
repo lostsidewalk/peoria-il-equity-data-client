64dad03e986f03af65133334ccaebb5416c3c4b2
Michael Harris <meh@lostsidealk.com>
2023-10-17 11:12:13 -0500

    Initial commit

---
+++ src/components/PeapList.vue
@@ -0,0 +1,13 @@
+<template>
+    PEAP List
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+    const response = await fetch('https://peoria-il-equity-data-api.onrender.com/peap');
+    const data = await response.json();
+    console.debug("PEAP list mounted, data=" + JSON.stringify(data));
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="home">
+    <PeapList />
+  </div>
+</template>
+
+<script setup>
+import PeapList from '../components/PeapList.vue';
+</script>
